13f7b2af8be91dff85380e7a5720909ddc55b0df
zhfgod <131757450+zhfgod@users.noreply.github.com>
2024-01-22 08:59:04 +0800

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,9 +1,9 @@
 module.exports = [{
-  userName: process.env.TY_USER_NAME || 'userName',
-  password: process.env.TY_PASSWORD || 'password',
+  userName: process.env.TY_USER_NAME || '18967702272',
+  password: process.env.TY_PASSWORD || '!Zhfgod1025',
 }, {
-  userName: process.env.TY_USER_NAME1,
-  password: process.env.TY_PASSWORD1,
+  userName: process.env.TY_USER_NAME1|| '13758710010',
+  password: process.env.TY_PASSWORD1,|| '!Zhfgod1025',
 }, {
   userName: process.env.TY_USER_NAME2,
   password: process.env.TY_PASSWORD2,
