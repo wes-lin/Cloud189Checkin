a8bd99a0eb93792c5fd46b5d6fb7e72fa0d083ef
Wes Lin <991184480@qq.com>
2024-04-25 02:02:14 +0000

    添加TV签到

--- accounts.js
+++ accounts.js
@@ -1,16 +1,21 @@
 module.exports = [{
   userName: process.env.TY_USER_NAME || 'userName',
   password: process.env.TY_PASSWORD || 'password',
+  familyId: process.env.TY_FAMILY_ID
 }, {
   userName: process.env.TY_USER_NAME1,
   password: process.env.TY_PASSWORD1,
+  familyId: process.env.TY_FAMILY_ID1
 }, {
   userName: process.env.TY_USER_NAME2,
   password: process.env.TY_PASSWORD2,
+  familyId: process.env.TY_FAMILY_ID2
 }, {
   userName: process.env.TY_USER_NAME3,
   password: process.env.TY_PASSWORD3,
+  familyId: process.env.TY_FAMILY_ID3
 }, {
   userName: process.env.TY_USER_NAME4,
   password: process.env.TY_PASSWORD4,
+  familyId: process.env.TY_FAMILY_ID4
 }];
