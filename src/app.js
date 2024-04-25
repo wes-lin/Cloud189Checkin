60b7a56c12bb6a8d9f26f1148cff797d377b6667
Wes Lin <991184480@qq.com>
2024-04-25 04:30:22 +0000

    测试

--- src/app.js
+++ src/app.js
@@ -195,7 +195,7 @@ const doTask = async (familyId) => {
     `https://cloud.189.cn/mkt/userSign.action?rand=${new Date().getTime()}&clientType=TELEANDROID&version=${config.version}&model=${config.model}`,
     'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN',
     'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN',
-    'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_2022_FLDFS_KJ&activityId=ACT_SIGNIN',
+    'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_2022_FLDFS_KJ&activityId=ACT_SIGNIN'
   ];
 
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
