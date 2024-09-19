a683cf8decb17d7d3a56ca2bc6a73eb0bf48c907
bafangseo <71722579+bafangseo@users.noreply.github.com>
2024-09-19 14:55:44 +0800

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,5 @@
 module.exports = {
   appToken:
     process.env.WX_PUSHER_APP_TOKEN || "AT_w1itT0TIo2dmEOZNd2HEctM6qsRVQED3",
-  uid: process.env.WX_PUSHER_UID || "",
+  uid: process.env.WX_PUSHER_UID || "UID_4847Fn0XJ53ELn7ZQsSe1nVlZJMa",
 };
