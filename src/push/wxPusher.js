6ea30fee47561e5ad6f3271f3695c5dc30df534d
sun558 <78543028+sun558@users.noreply.github.com>
2025-01-10 17:37:16 +0800

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,5 @@
 module.exports = {
   appToken:
-    process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
+    process.env.WX_PUSHER_APP_TOKEN || "AT_VsLIJtd7t88HyUbZsVPnXu1oXU0aqlrc",
   uid: process.env.WX_PUSHER_UID || "",
 };
