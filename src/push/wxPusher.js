a57d3611a9a3bfd6174386a18a76ab16fa45f39b
tianyiyun123 <183097527@qq.com>
2025-01-19 08:40:54 +0800

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,5 @@
 module.exports = {
   appToken:
     process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
-  uid: process.env.WX_PUSHER_UID || "",
+  uid: process.env.WX_PUSHER_UID || "UID_515ENQo5aFZCOak8kkF8VTh7Va5p",
 };
