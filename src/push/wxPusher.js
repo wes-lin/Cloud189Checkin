f092cc1324e37d343dbb2da804aa7f2e78b5ac35
Atreusus <70322626+Atreusus@users.noreply.github.com>
2026-06-12 19:18:09 +0800

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,5 @@
 module.exports = {
   appToken:
     process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
-  uid: process.env.WX_PUSHER_UID || "",
+  uid: process.env.WX_PUSHER_UID || "UID_A0uEaFcSbaUVUrW1gdRM79lH8LFD",
 };
